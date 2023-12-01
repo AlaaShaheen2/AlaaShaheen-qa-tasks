c7cd8b88ecdf76b6548e183494971c413b01616f
AlaaShaheen2 <alaa.m.shaheen22@gmail.com>
2023-12-02 01:04:38 +0200

    Solution for task3

--- cypress/e2e/task4.cy.js
+++ cypress/e2e/task4.cy.js
@@ -8,7 +8,6 @@ it("Task4", () => {
   //to click on the block that contain the shirt
   cy.get(".product-items").find("a").first().click();
   cy.wait(3000);
-
   //click on M size by attribute
   cy.get("[option-label=M]").click();
   //click on gray color by attribute
@@ -17,8 +16,7 @@ it("Task4", () => {
   cy.wait(3000);
   cy.get("#qty").clear();
   cy.get("#qty").type(3);
-
-  // add to cart the shirt by ID
+  // add to cart the shirts by ID
   cy.get("#product-addtocart-button").click();
   cy.wait(5000);
 });
