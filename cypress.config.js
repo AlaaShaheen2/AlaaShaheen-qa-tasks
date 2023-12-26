a40ae6584ff94e5bd71271601f471506ff1a99de
AlaaShaheen2 <alaa.m.shaheen22@gmail.com>
2023-12-27 00:11:41 +0200

    install cucumber

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,33 @@
 /// <reference types= "cypress" />
 
 const { defineConfig } = require("cypress");
+const cucumber = require("cypress-cucumber-preprocessor").default;
 
 module.exports = defineConfig({
   e2e: {
+    specPattern: "**/*.feature",
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      on("file:preprocessor", cucumber());
     },
     baseUrl: "https://magento.softwaretestingboard.com/",
-    // experimentalStudio:true, 
+    // experimentalStudio:true,
     // trashAssetsBeforeRuns:true,
     // screenshotOnRunFailure:true,
+    experimentalStudio: true,
+    trashAssetsBeforeRuns: true,
+    screenshotOnRunFailure: false,
+    screenshotsFolder: "MyScreenshots",
+    video: false,
+    videosFolder: "MyVideos",
+    videoCompression: 0,
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "cypress/myReport", // where to save in directory
+      overwrite: false,
+      html: true,
+      json: false,
+      timestamp: "mmddyyyy_HHMMss",
+    },
   },
 });
