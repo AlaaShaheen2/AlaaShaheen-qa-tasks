74c64636d81386c3aed01897dc22c4f5fd6cf277
AlaaShaheen2 <alaa.m.shaheen22@gmail.com>
2023-12-29 21:33:21 +0200

    Add a new Campaigns

--- cypress/e2e/cucumber/Tests/newCampaignsTest/newCampaignsTest.cy.js
+++ cypress/e2e/cucumber/Tests/newCampaignsTest/newCampaignsTest.cy.js
@@ -8,14 +8,31 @@ Given("The user click on campaigns button", () => {
   cy.contains("Campaigns").click();
 });
 
-When("Click on Add New button", () => {});
+When("Click on Add New button", () => {
+  cy.get("a.btn.btn-primary").click();
+});
 
-When("Enter name in name input field", () => {});
+When("Enter name in name input field", () => {
+  cy.get("#Name").clear().type("Holiday");
+});
 
-When("Enter subject in subject input field", () => {});
+When("Enter subject in subject input field", () => {
+  cy.get("#Subject").clear().type("Holiday discounts");
+});
 
-When("Enter body in body field", () => {});
+When("Enter body in body field", () => {
+  cy.get("#Body")
+    .clear()
+    .type("discount on different sections :Chocolate, Candy ,Drinks ... etc");
+});
 
-When("Click on Save button ", () => {});
+When("Click on Save button ", () => {
+  cy.get("[name=save]").click();
+});
 
-Then("The new campaigns should be added successfully", () => {});
+Then("The new campaigns should be added successfully", () => {
+  cy.get(".alert-dismissable").should(
+    "contain",
+    "The new campaign has been added successfully"
+  );
+});
